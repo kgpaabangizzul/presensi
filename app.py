3be6e54efd1bbbf70b9a0a69c2ee8f13a3fe4d13
kgpaabangizzul <bangjul.bisnis@gmail.com>
2026-03-10 10:00:15 +0700

    update port

--- app.py
+++ app.py
@@ -765,4 +765,4 @@ def api_shift_by_dept(dept_id):
     conn.close(); return jsonify([dict(s) for s in shifts])
 
 if __name__=='__main__':
-    init_db(); app.run(debug=True, host='0.0.0.0',port=5000)
+    init_db(); app.run(debug=True, host='0.0.0.0',port=5020)
